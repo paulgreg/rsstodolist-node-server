303e07c6b8aa838778f7d6a83cb10c403f69eb40
Grégory Paul <paulgreg@gmail.com>
2020-05-31 21:27:18 +0200

    Send a user-agent to avoid some basic bot detection

--- src/index.mjs
+++ src/index.mjs
@@ -111,6 +111,10 @@ sequelize
                       axios
                           .get(url, {
                               responseType: 'arraybuffer',
+                              headers: {
+                                  'User-Agent':
+                                      'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
+                              },
                           })
                           .then(function (response = {}) {
                               const { status, data } = response
