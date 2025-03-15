b0787ab6768ac235c274dae6bd4a5e18cc327d8c
Grégory Paul <me@gpaul.me>
2025-03-15 17:50:08 +0100

    chore: update pm2.sh

--- pm2.sh
+++ pm2.sh
@@ -1,1 +1,1 @@
-NODE_ENV=production pm2 start . --name 'rsstodolist-server' --max-memory-restart 128M
+NODE_ENV=production pm2 start dist/index.js --name 'rsstodolist-server' --max-memory-restart 128M
